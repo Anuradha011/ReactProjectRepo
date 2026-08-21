10d74ed3a01865f81f9e64fb9503266abc8e2980
Anuradha011 <thisisanuradha.prad@gmail.com>
2026-08-21 13:26:40 +0530

    Fix React project folders

---
+++ 02-component/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../components/Card'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <navbar />
+      </div>
+      {Card()}
+    </div>
+  )
+}
+
+export default App
