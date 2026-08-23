754bbb1705b630de46db4b95a54a0a6d0fd170a4
Anuradha011 <thisisanuradha.prad@gmail.com>
2026-08-23 14:11:37 +0530

    react file updated

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -9,31 +9,14 @@ const App = () => {
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn_cenbqs9kLcwYrlp3vn6JAV2X_M8mUm_-EZLsRetcg&s=10" alt="" />
           <button>Save <Bookmark size={12} /> </button>
 
-        </div>
-        <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
-
-        </div>
-        <div>
-           <h4>part time</h4>
-           <p>Senior Level</p>
-           </div> 
         
-        <div className='bottom'></div>
-         <div>
-           
-        <div>
-
-           <h3>120/hr</h3>
-           <p>Mumbai , India</p>
 
             </div>
          </div>
 
       </div>
 
-    </div>
+    
   )
 }
 
