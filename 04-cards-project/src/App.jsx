1cd3a1d577c79401bd6fbfab3d2c9b999d2a9841
Anuradha011 <thisisanuradha.prad@gmail.com>
2026-08-24 12:04:47 +0530

    app fie updated

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import {Bookmark} from "lucide-react"
+import Bookmark from 'lucide-react'
 
 const App = () => {
   return (
     <div className='parent'>
       <div className="card">
         <div className="top">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn_cenbqs9kLcwYrlp3vn6JAV2X_M8mUm_-EZLsRetcg&s=10" alt="" />
-          <button>Save <Bookmark size={12} /> </button>
-
-        
-
-            </div>
-         </div>
-
+          <img src="https://static.startuptalky.com/2021/07/Amazon-Logo-StartupTalky.jpg" alt="" />
+          <button>Save <Bookmark /> </button>
       </div>
-
+      <div className="center">
+         <h3>Amazon <span>5 days ago</span></h3>
+         <h2>Senior UI/UX Designer</h2> 
+      </div>
+      <div className="bottom">
+        
+      </div>
+     </div>
+    </div>
     
   )
 }
